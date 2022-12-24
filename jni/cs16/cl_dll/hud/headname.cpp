d91c78788b448fd9df9257480fdfd13c86722087
hasandramali <93338836+hasandramali@users.noreply.github.com>
2022-12-24 20:24:19 +0200

    add headname

---
+++ jni/cs16/cl_dll/hud/headname.cpp
@@ -0,0 +1,68 @@
+#include "hud.h"
+#include "cl_util.h"
+#include "cl_entity.h"
+#include "triangleapi.h"
+#include "draw_util.h"
+#include "com_model.h"
+#include "calcscreen.h"
+
+int CHudHeadName::Init(void)
+{
+	gHUD.AddHudElem(this);
+
+	m_iFlags |= HUD_DRAW;
+
+	return 1;
+}
+
+int CHudHeadName::VidInit(void)
+{
+	return 1;
+}
+
+bool CHudHeadName::CheckForPlayer(cl_entity_s *pEnt)
+{
+	if (pEnt && pEnt->model && pEnt->model->name[0] && pEnt->player)
+		return true;
+
+	return false;
+}
+
+int CHudHeadName::Draw(float flTime)
+{
+	if ((gHUD.m_iHideHUDDisplay & HIDEHUD_ALL) || g_iUser1 || !gHUD.cl_headname->value)
+		return 1;
+
+	for (int i = 1; i < 33; i++)
+	{
+		if (g_PlayerExtraInfo[i].dead)
+			continue;
+
+		if (g_PlayerExtraInfo[i].teamnumber != g_PlayerExtraInfo[gHUD.m_Scoreboard.m_iPlayerNum].teamnumber)
+			continue;
+
+		if (i != gHUD.m_Scoreboard.m_iPlayerNum)
+		{
+			cl_entity_t *ent = gEngfuncs.GetEntityByIndex(i);
+
+			if (!CheckForPlayer(ent))
+				continue;
+
+			model_t *model = ent->model;
+			vec3_t origin = ent->origin;
+
+			if (model)
+				origin.z += max(model->maxs.z, 35.0);
+
+			float screen[2]{ -1,-1 };
+			if (!CalcScreen(origin, screen))
+				continue;
+
+			int textlen = DrawUtils::HudStringLen(g_PlayerInfoList[i].name);
+
+			DrawUtils::DrawHudString(screen[0] - textlen * 0.5f, screen[1], gHUD.m_scrinfo.iWidth, g_PlayerInfoList[i].name, 150, 150, 150);
+		}
+	}
+
+	return 1;
+}
