7361ec4f0003871edaf1b6eadf558c95c0ca8aba
hasandramali <93338836+hasandramali@users.noreply.github.com>
2023-02-20 22:49:17 +0200

    Update cbase.h

--- jni/cs16/3rdparty/regamelite/dlls/cbase.h
+++ jni/cs16/3rdparty/regamelite/dlls/cbase.h
@@ -264,6 +264,8 @@ public:
 	CBaseEntity *m_pChildMoveWith;
 	float m_fPevNextThink;
 	float m_fNextThink;
+	virtual void SetNextThink( float delay ) { SetNextThink(delay, FALSE); }
+	virtual void SetNextThink( float delay, BOOL correctSpeed );
 
 #ifndef REGAMEDLL_FIXES
 	virtual void StopSneaking() {}
@@ -394,6 +396,10 @@ public:
 	void (CBaseEntity::*m_pfnTouch)(CBaseEntity *pOther);
 	void (CBaseEntity::*m_pfnUse)(CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value);
 	void (CBaseEntity::*m_pfnBlocked)(CBaseEntity *pOther);
+	void (CBaseEntity::*m_pMoveWith)();
+	void (CBaseEntity::*m_pChildMoveWith)();
+	void (CBaseEntity::*m_fPevNextThink)(float value);
+	void (CBaseEntity::*m_fNextThink)(float value);
 
 #ifdef REGAMEDLL_API
 	CCSEntity *m_pEntity;
