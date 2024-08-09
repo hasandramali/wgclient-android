81c40918da3a1d9899a0d476aebbc50c7cc90c7f
hasandramali <93338836+hasandramali@users.noreply.github.com>
2024-08-09 21:21:52 +0300

    Update Application.mk

--- jni/Application.mk
+++ jni/Application.mk
@@ -2,7 +2,8 @@ CFLAGS_OPT :=  -O3 -fomit-frame-pointer -ggdb -funsafe-math-optimizations -ftree
 CFLAGS_OPT_ARM := -mthumb -mfpu=neon -mcpu=cortex-a9 -pipe -mvectorize-with-neon-quad -DVECTORIZE_SINCOS
 CFLAGS_OPT_ARMv5 :=-march=armv6 -mfpu=vfp -marm -pipe
 CFLAGS_OPT_X86 := -mtune=atom -march=atom -mssse3 -mfpmath=sse -funroll-loops -pipe -DVECTORIZE_SINCOS
-CFLAGS_OPT_ARM64 := -mcpu=cortex-a53 -DVECTORIZE_SINCOS -pipe
+CFLAGS_OPT_ARM64 := -mcpu=cortex-a53 -march=armv8-a -pipe -O3 -fomit-frame-pointer -funsafe-math-optimizations \
+                    -ftree-vectorize -floop-interchange -finline-limit=256 -ffast-math -funroll-loops
 CFLAGS_HARDFP := -D_NDK_MATH_NO_SOFTFP=1 -mhard-float -mfloat-abi=hard -DLOAD_HARDFP -DSOFTFP_LINK
 APPLICATIONMK_PATH = $(call my-dir)
 
